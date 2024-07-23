a093268acc98b4a2d71add002d8c5f9cd338185b
AmnaNehal <128274838+AmnaNehal@users.noreply.github.com>
2024-07-24 00:00:18 +0500

    Add files via upload

---
+++ PROJECT_02_NUMBER-GUESSING-GAME/index.ts
@@ -0,0 +1,20 @@
+import inquirer from "inquirer";
+
+console.log("\t\t\t\t NUMBER GUESSING GAME ");
+const randomNumber = Math.floor(Math.random()*10+1)
+
+const answers = await inquirer.prompt([
+    {
+        message: "Please guess a number between 1 and 10 : ",
+        name: "userGuessNumber",
+        type: "input",
+    },
+]);
+
+const userGuessNumber = parseInt(answers.userGuessNumber, 10);  // Convert the input to a number
+
+if (userGuessNumber === randomNumber) {
+    console.log("Congratulations! You guessed the right number.");
+} else {
+    console.log("You guessed the wrong number.");
+}
